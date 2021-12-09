6095cf19aac970745b5432664ab9abefb800e65a
Nhan Huynh <huynhnhan.dev@gmail.com>
2021-12-09 21:44:28 +0700

    add 0 0 test

--- sum.test.js
+++ sum.test.js
@@ -1,8 +1,12 @@
-import { expect } from "@jest/globals";
-import { sum } from "./sum";
-test("adds 1 + 2 to equal 3", () => {
+import { expect } from '@jest/globals';
+import { sum } from './sum';
+test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
-test("adds 14 + 2 to equal 16", () => {
+test('adds 14 + 2 to equal 16', () => {
   expect(sum(14, 2)).toBe(16);
+});
+
+test('0, 0', () => {
+  expect(sum(0, 0)).toBe(0);
 });
