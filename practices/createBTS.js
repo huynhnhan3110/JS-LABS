5c7cd27eaeb1e3d9fbb35c5c4feba989dac241c0
Huynh Nhan Huu <huynhnhan.dev@gmail.com>
2022-02-04 20:42:24 +0700

    bts search findmin getHeight

--- practices/createBTS.js
+++ practices/createBTS.js
@@ -23,11 +23,45 @@ function createBTS(numberList) {
 
     return root;
   }
+  function findMin(node) {
+    if (node == null) return null;
+    let minNode = node;
+    while (minNode.left != null) {
+      minNode = minNode.left;
+    }
+    return minNode;
+  }
+  function search(node, key) {
+    if (node == null) return null;
+    if (key > node.key) return search(node.right, key);
+    if (key < node.key) return search(node.left, key);
 
+    return node;
+  }
+
+  function getMaxNodesCount(node) {
+    if (node == null) return 0;
+    const leftHeight = getMaxNodesCount(node.left);
+    const rightHeight = getMaxNodesCount(node.right);
+
+    return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+  }
+  function getHeight(node) {
+    const height = getMaxNodesCount(node);
+    return height > 0 ? height - 1 : 0;
+  }
   return {
     root,
     insert,
+    findMin,
+    search,
+    getMaxNodesCount,
+    getHeight,
   };
 }
-const bts = createBTS([5, 2, 6, 1, 7, 1]);
+const bts = createBTS([5, 2]);
 console.log(bts.root);
+console.log(bts.findMin(bts.root));
+console.log(bts.search(bts.root, 5));
+console.log(bts.getMaxNodesCount(bts.root));
+console.log(bts.getHeight(bts.root));
